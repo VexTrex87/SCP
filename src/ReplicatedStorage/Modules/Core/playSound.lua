dad3f1578e4fc5e81b96a20de4044bf3c1dafba5
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-12-17 00:04:31 -0500

    Removed unequip sound
    When unequipping a tool, the tool would be parented to the backpack where the sound would not be heared

--- src/ReplicatedStorage/Modules/Core/playSound.lua
+++ src/ReplicatedStorage/Modules/Core/playSound.lua
@@ -1,6 +1,10 @@
 local Debris = game:GetService("Debris")
 
 return function(sound, newSoundParent, despawnDelay)
+	if not sound and newSoundParent then
+		return
+	end
+	
 	local newSound = sound:Clone()
 	newSound.Parent = newSoundParent
 	newSound:Play()
