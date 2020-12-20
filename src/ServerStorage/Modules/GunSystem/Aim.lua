c0abb216eba00186d55aeb6590062e27cc7eab53
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-12-20 16:54:55 -0500

    Fixed incorrect module name

--- src/ServerStorage/Modules/GunSystem/Aim.lua
+++ src/ServerStorage/Modules/GunSystem/Aim.lua
@@ -5,7 +5,6 @@ local newThread = require(core.NewThread)
 local playSound = require(core.PlaySound)
 
 return function(self, willAim: boolean, willZoom: boolean)
-    print(willAim and self.sounds.equip or self.sounds.unequip)
     newThread(playSound, willAim and self.sounds.equip, self.handle)
     self.Configuration.bullet.spread.currentMin = willZoom and self.Configuration.bullet.spread.ADSMin or self.Configuration.bullet.spread.unaimedMin
     self.Configuration.bullet.spread.currentMax = willZoom and self.Configuration.bullet.spread.ADSMax or self.Configuration.bullet.spread.unaimedMax
