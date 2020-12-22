7e37ea0fce9be12b0394ecc5289068ce0c1e7c7c
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-12-22 11:39:41 -0500

    Fixed formatting in movement & images configuration
    Closes #79

--- src/ReplicatedStorage/Configuration/Images.lua
+++ src/ReplicatedStorage/Configuration/Images.lua
@@ -1,10 +1,10 @@
 return {
 	gunIcons = {
-		desert_eagle = "rbxassetid://6048706726",
-		glock_19 = "rbxassetid://6048706785",
-		m249 = "rbxassetid://6048706837",
-		m4a1 = "rbxassetid://6048706895",
+		["desert_eagle"] = "rbxassetid://6048706726",
+		["glock_19"] = "rbxassetid://6048706785",
+		["m249"] = "rbxassetid://6048706837",
+		["m4a1"] = "rbxassetid://6048706895",
 		["pp-19_bizon"] = "rbxassetid://6048706955",
-		ump45 = "rbxassetid://6048707006",
+		["ump45"] = "rbxassetid://6048707006",
 	}
 }
--- src/ReplicatedStorage/Configuration/Movement.lua
+++ src/ReplicatedStorage/Configuration/Movement.lua
@@ -4,18 +4,15 @@ return {
         walk = 5,
         crouch = 2,
     },
-
     fov = {
         sprint = 70 + (16 / 5),
         walk = 70,
         crouch = 70 - (2 / 5),
     },
-
     keybinds = {
         sprint = Enum.KeyCode.LeftShift,
         crouch = Enum.KeyCode.LeftControl,
         roll = Enum.KeyCode.C,
     },
-
     povTweenInfo = TweenInfo.new(0.4, Enum.EasingStyle.Sine)
 }
