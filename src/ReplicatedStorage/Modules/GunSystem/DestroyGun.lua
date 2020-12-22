1375323ba7c3278cb3bcdd705e2a08845cc307e3
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-12-22 11:32:20 -0500

    Remove unnecessary print & warn messages
    Closes #70

--- src/ReplicatedStorage/Modules/GunSystem/DestroyGun.lua
+++ src/ReplicatedStorage/Modules/GunSystem/DestroyGun.lua
@@ -2,6 +2,5 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local disconnectConnections = require(ReplicatedStorage.Modules.Core.DisconnectConnections)
 
 return function(self)
-    print("person died on client")
     disconnectConnections(self.temp.connections)
 end
--- src/ServerStorage/Modules/GunSystem/DestroyGun.lua
+++ src/ServerStorage/Modules/GunSystem/DestroyGun.lua
@@ -2,6 +2,5 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local disconnectConnections = require(ReplicatedStorage.Modules.Core.DisconnectConnections)
 
 return function(self)
-    print("player died on server")
     disconnectConnections(self.temp.connections)
 end
