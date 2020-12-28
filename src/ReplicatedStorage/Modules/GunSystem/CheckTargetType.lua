e1634683f9eb7ffe1c44dce50ae0b8bff8bfe389
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-12-27 21:17:00 -0500

    made players damage other players

--- src/ReplicatedStorage/Modules/GunSystem/CheckTargetType.lua
+++ src/ReplicatedStorage/Modules/GunSystem/CheckTargetType.lua
@@ -1,9 +1,7 @@
 return function(character)
     if not character then
         return "NEUTRAL"
-    elseif character.Name == "Friendly" then
-        return "FRIENDLY"
-    elseif character.Name == "Enemy" then
+    else
         return "ENEMY"
     end
 end
